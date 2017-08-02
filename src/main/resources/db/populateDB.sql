d9a078c9774ee20a4a1d93b362942f8f960ed83a
Andrey Avtuchovich <andrey.avtuchovich@gmail.com>
2017-08-02 22:31:04 +0300

    fixed visible test classes in main folder

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,3 +11,6 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, desription, calories) VALUES
+  ('31.05.2015 13:55','обед',1200);
