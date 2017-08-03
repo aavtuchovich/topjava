231bd891ad9470ac20bacb57ea7e8abca90bbdfb
Andrey Avtuchovich <andrey.avtuchovich@gmail.com>
2017-08-03 15:16:47 +0300

    Revert "complete prepare to hw3"
    This reverts commit 9c5f9dcdbf012c62452d8dd3da60904fe8323f89.

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -1,18 +1,11 @@
 package ru.javawebinar.topjava;
 
-import ru.javawebinar.topjava.model.BaseEntity;
-
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class AuthorizedUser {
-    private static int id = BaseEntity.START_SEQ;
 
     public static int id() {
-        return id;
-    }
-
-    public static void setId(int id) {
-        AuthorizedUser.id = id;
+        return 1;
     }
 
     public static int getCaloriesPerDay() {
