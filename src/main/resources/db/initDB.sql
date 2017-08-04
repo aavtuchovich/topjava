985a2dca829b26fbd0d208a8eddde5e9943ba56c
Andrey Avtuchovich <andrey.avtuchovich@gmail.com>
2017-08-04 12:50:05 +0300

    moved test classes to test directory added script for drop-create table meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -26,5 +27,9 @@ CREATE TABLE user_roles
 );
 
 CREATE TABLE meals (
-  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq')
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id     INTEGER REFERENCES users (id),
+  description VARCHAR,
+  dateTime    TIMESTAMP,
+  calories    INTEGER
 );
