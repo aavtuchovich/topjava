a5cfb2b6ee5c29f5236db2793fe315d8f5d7e66b
Andrey Avtuchovich <andrey.avtuchovich@gmail.com>
2017-08-08 16:42:56 +0300

    add meal entity

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -2,36 +2,53 @@ package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional
 public class JpaMealRepositoryImpl implements MealRepository {
 
-    @Override
-    public Meal save(Meal meal, int userId) {
-        return null;
-    }
+	@PersistenceContext
+	private EntityManager em;
 
-    @Override
-    public boolean delete(int id, int userId) {
-        return false;
-    }
+	@Override
+	@Transactional
+	public Meal save(Meal meal, int userId) {
+		User ref = em.getReference(User.class, userId);
+		meal.setUser(ref);
+		if (meal.isNew()) {
+			em.persist(meal);
+			return meal;
+		} else {
+			return em.merge(meal);
+		}
+	}
 
-    @Override
-    public Meal get(int id, int userId) {
-        return null;
-    }
+	@Override
+	@Transactional
+	public boolean delete(int id, int userId) {
+		return false;
+	}
 
-    @Override
-    public List<Meal> getAll(int userId) {
-        return null;
-    }
+	@Override
+	public Meal get(int id, int userId) {
+		return null;
+	}
 
-    @Override
-    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
-    }
+	@Override
+	public List<Meal> getAll(int userId) {
+		return null;
+	}
+
+	@Override
+	public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+		return null;
+	}
 }
