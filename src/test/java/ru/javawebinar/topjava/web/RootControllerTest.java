4548ec2a0effb04c062bc7b87f9a8f641696ae1c
Andrey Avtuchovich <andrey.avtuchovich@gmail.com>
2017-09-21 23:10:21 +0300

    added mvc test to meals and test to css resources

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -11,19 +11,29 @@ import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class RootControllerTest extends AbstractControllerTest {
 
-    @Test
-    public void testUsers() throws Exception {
-        mockMvc.perform(get("/users"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("users"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
-                .andExpect(model().attribute("users", hasSize(2)))
-                .andExpect(model().attribute("users", hasItem(
-                        allOf(
-                                hasProperty("id", is(START_SEQ)),
-                                hasProperty("name", is(USER.getName()))
-                        )
-                )));
-    }
+	@Test
+	public void testUsers() throws Exception {
+		mockMvc.perform(get("/users"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("users"))
+				.andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+				.andExpect(model().attribute("users", hasSize(2)))
+				.andExpect(model().attribute("users", hasItem(
+						allOf(
+								hasProperty("id", is(START_SEQ)),
+								hasProperty("name", is(USER.getName()))
+						)
+				)));
+	}
+
+	@Test
+	public void testMeals() throws Exception {
+		mockMvc.perform(get("/meals"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("meals"))
+				.andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+				.andExpect(model().attribute("meals", hasSize(6)));
+	}
 }
